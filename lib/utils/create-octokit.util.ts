67293752d3116a5ad7736bf7e36d8b6968b5d42e
Akshay Singh <8209379+akshayKrSingh@users.noreply.github.com>
2021-10-20 16:11:45 +0530

    feat(octokit): add ability to pass auth options

--- lib/utils/create-octokit.util.ts
+++ lib/utils/create-octokit.util.ts
@@ -2,12 +2,13 @@ import { Octokit } from '@octokit/rest';
 import { loadConfigUtil } from './load-config.util';
 import { createAppAuth } from '@octokit/auth-app';
 
-export const createOctokit = () => {
+export const createOctokit = (authOptions: any) => {
   const config = loadConfigUtil();
   return new Octokit({
     authStrategy: createAppAuth,
     baseUrl: config.ghUrl,
     auth: {
+      ...authOptions,
       appId: config.appId,
       privateKey: config.privateKey,
       clientId: config.ghClientId,
